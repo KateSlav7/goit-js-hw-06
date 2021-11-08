0fd777286558c27e320a3997df6a1591c9492142
KateSlav7 <kateslavgorodskaya@gmail.com>
2021-11-08 14:56:26 +0200

    commit_js_files

---
+++ js/task-02.js
@@ -0,0 +1,18 @@
+const ingredients = [
+    "Potatoes",
+    "Mushrooms",
+    "Garlic",
+    "Tomatos",
+    "Herbs",
+    "Condiments",
+  ];
+
+const ulEl = document.getElementById("ingredients");
+const elements = ingredients.map((ingredient) => {
+const ingredientsEl = document.createElement("li");
+    ingredientsEl.classList.add("item");
+    ingredientsEl.textContent = ingredient;
+    return ingredientsEl;
+  });
+  
+ulEl.append(...elements);
